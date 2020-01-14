9bf163bb876d53806640f44578184d1bca825e98
Farooque Ahmed <Farooque.FB@outlook.com>
2020-01-14 18:33:39 +0500

    Add project files.

---
+++ nested if else/Program.cs
@@ -0,0 +1,366 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nested_if_else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int sanghar = 100;
+            int Umerkot = 200;
+
+            Console.Write("Enter distance ");
+            int.Parse(Console.ReadLine());
+            //Console.Write("Enter Umerkot distance ");
+            //Umerkot = int.Parse(Console.ReadLine());
+
+            if (sanghar == 120 && Umerkot == 200 )
+            
+              
+                {
+                    Console.WriteLine("your destinatinon is sanghar");
+                }
+
+                else
+                {
+                    Console.WriteLine("Your way is towards Umerkot");
+                }
+
+
+                /*
+             int phonenumber = 222;
+             int whtsapp = 333;
+
+
+             Console.Write("Diall phonenumber here ");
+             phonenumber = int.Parse(Console.ReadLine());
+             Console.Write("Diall whtsapp number ");
+             whtsapp = int.Parse(Console.ReadLine());
+
+
+             if (phonenumber != 222)
+             {
+                 if (whtsapp == 333)
+                 {
+                     Console.WriteLine("Hello How r u ");
+                 }
+                 else
+                 {
+                     Console.WriteLine("Sorry you have dialled wrong number");
+                 }
+
+             }
+             else
+             {
+                 Console.WriteLine("Good morning sweet hard");
+             }
+
+
+
+
+
+             /*
+             string username = "Safe";
+             string password = "Zafar";
+
+             Console.WriteLine("please enter your username");
+             username = Console.ReadLine();
+             Console.WriteLine("Please enter you password");
+             password = Console.ReadLine();
+
+             if(username == "safe" || password == "zafar")
+             {
+                 Console.WriteLine("You are wellcome to our page");
+             }
+
+             else
+             {
+                 Console.WriteLine("password or user name incorrect");
+             }
+
+
+
+
+             string studentname = "farooque";
+             int RollNo =  32;
+             Console.Write("Please enter your name ");
+             studentname = Console.ReadLine();
+             Console.Write("Please enter your Roll no ");
+             RollNo = int.Parse(Console.ReadLine());
+
+             if(studentname == "farooque" && RollNo == 32 )
+             {
+                 Console.WriteLine("He is computer science student ");
+             }
+
+             else
+             {
+                 Console.WriteLine(" the data does not match"  );
+             }
+
+
+
+
+
+
+
+
+              int marks = 55;
+              string Grade = "";
+
+
+
+               if (marks > 50 && marks < 60)
+              {
+                  Grade = "C";
+              }
+
+              else if (marks > 60 && marks < 70)
+              {
+                  Grade = "B";
+              }
+
+              else if (marks > 70 && marks < 80)
+
+              {
+                  Grade = "A";
+
+              }
+
+              else if (marks > 80 && marks < 100)
+              {
+                  Grade = "A+";
+              }
+
+              Console.WriteLine("Your Grade is {0}",Grade);
+              Console.ReadLine();
+
+             int age;
+              int money;
+
+              Console.Write("Please enter your age: ");
+              age = int.Parse(Console.ReadLine());
+              Console.Write("Please enter money:");
+              money = int.Parse(Console.ReadLine());
+
+
+              if(age > 10 || money < 30) 
+              { 
+
+              //if(age < 10 && money < 30)
+              //{ 
+
+              //if (age > 10)
+              //{
+              //    if (money < 30)
+              //    {
+                      Console.WriteLine("Wellcome to my shop");
+
+                  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+              Console.Write("Enter any number :");
+              int a = int.Parse(Console.ReadLine());
+              if (a <= 10)
+              {
+                  if (a == 10)
+                  {
+                      Console.WriteLine("a is equal to 5");
+                  }
+                  else
+                  {
+                      Console.WriteLine("a is not less to 5 & not equal to 5");
+
+                  }
+              }
+              else
+              {
+                  Console.WriteLine("a is greater then 10");
+              }
+
+              if (a < 10)
+              {
+                  Console.WriteLine("a is less then 10");
+              }
+              else
+              if (a == 10)
+              {
+                  Console.WriteLine("a is equal to  10");
+              }
+              else
+              {
+                  Console.WriteLine(" a is greater then ");
+
+                  Console.ReadKey();
+
+
+
+
+
+
+
+
+                  int n = 10;
+                  int m = 20;
+
+                  if (m > 10)
+                  {
+                      if (n > 20)
+
+                      {
+                          Console.WriteLine("Result1");
+                      }
+
+
+                      else
+                      }
+
+                  Console.WriteLine("Result2");
+
+                  {
+
+
+
+
+
+
+
+
+
+
+                      int n = 10;
+                      int m = 20;
+                      if (n >= m)
+                      {
+                          if (n <= m)
+
+                          {
+                              Console.WriteLine(" x is less then y");
+                          }
+                          else
+                          {
+                              Console.WriteLine(" x is graeter then y");
+                          }
+                      }
+                      else
+                      {
+                          if (n < m)
+                          {
+                              Console.WriteLine(" result is true ");
+                          }
+                          else
+                          {
+                              Console.WriteLine("result is false");
+                          }
+
+
+
+
+
+
+
+                          int number = 3;
+                          int number1 = 4;
+                          int number2 = 8;
+
+                          if (number > number1)
+                          {
+                              if (number < number1)
+
+                              {
+                                  Console.WriteLine("{0} is largest", number2);
+                              }
+                              else
+                              {
+                                  Console.WriteLine("{0} is largest ", number1);
+                              }
+
+                          }
+                          else
+
+                          {
+                              if (number1 < number2)
+                              {
+                                  Console.WriteLine("{0} is less then", number2);
+
+                              }
+                              else
+                              {
+                                  Console.WriteLine("{0} is largest then", number2);
+
+                              }
+
+                              int x = 10;
+                              int y = 20;
+
+                              if (x > y)
+                              {
+                                  if (x < y)
+                                  {
+
+                                      Console.WriteLine(" x value is less then or equal  20");
+
+                                  }
+                                  else
+                                  {
+                                      Console.WriteLine("y value is greater  or less then 30");
+                                  }
+
+                              }
+                              else
+                              {
+                                  if (x >= y)
+
+                                  {
+                                      Console.WriteLine("x greater then or equal to y");
+
+                                  }
+
+
+                                  else
+                                  {
+                                      Console.WriteLine("x less then y");
+                                  }
+
+
+
+                                  Console.WriteLine("Press Enter Key to Exit..");
+
+                                  Console.ReadLine();
+                                  */
+            }
+        }
+    }
+
+
+
+
+        
+    
+
+
